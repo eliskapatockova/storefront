a1811bb43989725ec52948478f88350f06f94164
Eliska Patockova <patockovaeliska@gmail.com>
2020-05-04 11:18:39 -0700

    grammar fixes and error handling code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,8 +19,8 @@ var SQLconnection = mysql.createConnection({
   function shop () {
 
     SQLconnection.query("SELECT * FROM products", function(error, result){
-        console.log(result);
-        console.log(error);
+        //console.log(result);
+        //console.log(error);
         if(error){console.log("ERROR"); return;}
 
         console.log("Welcome to Bamazon! Here are the items we recommend for you: ")
@@ -30,12 +30,12 @@ var SQLconnection = mysql.createConnection({
             + "price: $" + result[i].price;
             console.log(currentItem);
         }
-
+        console.log("");
         inquirer.prompt([
             {
             type: "input",
 	  		name: "id",
-	  		message: "Enter the name of the product you'd like to buy:",
+	  		message: "Enter the ID of the product you'd like to buy:",
 	  		validate: function(value) {
 	  			if(isNaN(value) === false) {
 	  				return true;
@@ -62,12 +62,13 @@ var SQLconnection = mysql.createConnection({
 
             var counter;
             for(counter = 0; counter<result.length; counter++){
-                if (itemID === result[counter].item_id){break;}
+                //console.log(counter + ": " + result[counter].item_id);
+                if (itemID == result[counter].item_id){break;}
             }
             if(counter >= result.length){console.log("Invalid Item ID."); return;}
             if(quantityDesired > result[counter].stock_quantity){console.log("Insufficient stock."); return;}
 
-            SQLconnection.query("UPDATE products SET stock_quantity"= "stock_quantity - ? WHERE item_id = ?", [quantityDemanded, itemId]);
+            SQLconnection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantityDesired, itemID]);
             var amountDue = result[counter].price * quantityDesired;
             console.log("Your purchase cost $" + amountDue + ". Have a nice day!");
 
